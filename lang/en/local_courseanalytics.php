eb3bc0283cbb5d85764fa3f34721536a07f24d1d
Bastian Coquedano <bastiancoquedano@MacBook-Air-de-Bastian.local>
2026-02-25 01:15:14 -0300

    feat(i18n): add complete EN/ES language keys for dashboard and page

--- lang/en/local_courseanalytics.php
+++ lang/en/local_courseanalytics.php
@@ -2,3 +2,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Course analytics';
+$string['pagetitle'] = 'Course analytics';
+$string['pageheading'] = 'Course analytics';
+$string['dashboardtitle'] = 'Course engagement metrics';
+$string['totalcoursescreated'] = 'Total courses created';
+$string['totalenrolments'] = 'Total enrolments';
+$string['totalcompletions'] = 'Total completions';
+$string['dashboardemptystate'] = 'No engagement metrics are available yet.';
--- lang/es/local_courseanalytics.php
+++ lang/es/local_courseanalytics.php
@@ -2,3 +2,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Analitica del curso';
+$string['pagetitle'] = 'Analitica del curso';
+$string['pageheading'] = 'Analitica del curso';
+$string['dashboardtitle'] = 'Metricas de participacion del curso';
+$string['totalcoursescreated'] = 'Total de cursos creados';
+$string['totalenrolments'] = 'Total de matriculas';
+$string['totalcompletions'] = 'Total de finalizaciones';
+$string['dashboardemptystate'] = 'Aun no hay metricas de participacion disponibles.';
